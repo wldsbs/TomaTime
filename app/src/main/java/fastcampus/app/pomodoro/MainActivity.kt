bd5ff8beabebe1a24a42564282fa30b0e259bb30
jiyun <jiyun_99@naver.com>
2022-02-17 16:40:49 +0900

    타이머 기능 추가
    CountDownTimer를 이용하여 타이머 기능 추가

--- app/src/main/java/fastcampus/app/pomodoro/MainActivity.kt
+++ app/src/main/java/fastcampus/app/pomodoro/MainActivity.kt
@@ -2,19 +2,26 @@ package fastcampus.app.pomodoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.SeekBar
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     //나중에 remainMinutesTextView에 접근을 했을때 findViewById 실행해서 가져옴
-    private val remainMinutesTextView: TextView by lazy{
+    private val remainMinutesTextView: TextView by lazy {
         findViewById(R.id.remainMinutesTextView)
     }
 
-    private val seekBar: SeekBar by lazy{
+    private val remainSecondsTextView: TextView by lazy {
+        findViewById(R.id.remainSecondsTextView)
+    }
+
+    private val seekBar: SeekBar by lazy {
         findViewById(R.id.seekBar)
     }
+
+    private var currentCountDownTimer: CountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,22 +31,51 @@ class MainActivity : AppCompatActivity() {
     }
 
     //view에 있는 리스너와 실제 로직 연결
-    private fun bindViews(){
+    private fun bindViews() {
         seekBar.setOnSeekBarChangeListener(
-            object : SeekBar.OnSeekBarChangeListener{
+            object : SeekBar.OnSeekBarChangeListener {
                 override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                    remainMinutesTextView.text = "%02d".format(progress)
+                    if(fromUser){
+                        updateRemainTimes(progress * 60 * 1000L)
+                    }
                 }
 
                 override fun onStartTrackingTouch(seekBar: SeekBar?) {
-
+                    currentCountDownTimer?.cancel()
+                    currentCountDownTimer = null
                 }
 
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    seekBar ?: return
 
+                    currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
+                    currentCountDownTimer?.start()
                 }
             }
         )
+    }
 
+    private fun createCountDownTimer(initialMillis: Long) =
+        object : CountDownTimer(initialMillis, 1000L) {
+            override fun onTick(millisUntilFinished: Long) {
+                updateRemainTimes(millisUntilFinished)
+                updateSeekBar(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                updateRemainTimes(0)
+                updateSeekBar(0)
+            }
+        }
+
+    private fun updateRemainTimes(remainMillis: Long){
+        val remainSeconds = remainMillis / 1000
+
+        remainMinutesTextView.text = "%02d".format(remainSeconds / 60)
+        remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
+    }
+
+    private fun updateSeekBar(remainMillis: Long){
+        seekBar.progress = (remainMillis / 1000 / 60).toInt()
     }
 }
