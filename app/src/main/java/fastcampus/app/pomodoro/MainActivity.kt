ee4ad7f1b7d8699565f6e75649b878e95edb8c8e
jiyun <jiyun_99@naver.com>
2022-02-21 18:38:48 +0900

    효과음 추가

--- app/src/main/java/fastcampus/app/pomodoro/MainActivity.kt
+++ app/src/main/java/fastcampus/app/pomodoro/MainActivity.kt
@@ -1,5 +1,6 @@
 package fastcampus.app.pomodoro
 
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -21,13 +22,33 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.seekBar)
     }
 
+    private val soundPool = SoundPool.Builder().build()
+
     private var currentCountDownTimer: CountDownTimer? = null
+    private var tickingSoundId: Int? = null
+    private var bellSoundId: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         bindViews()
+        initSounds()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        soundPool.autoResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        soundPool.autoPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        soundPool.release()
     }
 
     //view에 있는 리스너와 실제 로직 연결
@@ -41,18 +62,25 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                    currentCountDownTimer?.cancel()
-                    currentCountDownTimer = null
+                    stopCountDown()
                 }
 
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {
                     seekBar ?: return
 
-                    currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
-                    currentCountDownTimer?.start()
+                    if(seekBar.progress == 0){
+                        stopCountDown()
+                    } else{
+                        startCountDown()
+                    }
                 }
             }
         )
+    }
+
+    private fun initSounds(){
+        tickingSoundId = soundPool.load(this, R.raw.timer_ticking, 1)
+        bellSoundId = soundPool.load(this, R.raw.timer_bell, 1)
     }
 
     private fun createCountDownTimer(initialMillis: Long) =
@@ -63,15 +91,39 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                updateRemainTimes(0)
-                updateSeekBar(0)
+                completeCountDown()
             }
         }
+    private fun startCountDown(){
+        currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
+        currentCountDownTimer?.start()
+
+        tickingSoundId?.let {soundId ->
+            soundPool.play(soundId,1F, 1F, 0, -1, 1F)
+        }
+    }
+
+    private fun stopCountDown(){
+        currentCountDownTimer?.cancel()
+        currentCountDownTimer = null
+        soundPool.autoPause()
+    }
+
+    private fun completeCountDown(){
+        updateRemainTimes(0)
+        updateSeekBar(0)
+
+        soundPool.autoPause()
+        bellSoundId?.let{ soundId ->
+            soundPool.play(soundId, 1F, 1F, 0, -1, 1F)
+
+        }
+    }
 
     private fun updateRemainTimes(remainMillis: Long){
         val remainSeconds = remainMillis / 1000
 
-        remainMinutesTextView.text = "%02d".format(remainSeconds / 60)
+        remainMinutesTextView.text = "%02d'".format(remainSeconds / 60)
         remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
     }
 
