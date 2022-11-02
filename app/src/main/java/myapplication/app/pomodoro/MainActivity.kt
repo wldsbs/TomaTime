991820b213cc65d7904c6c73465ff98118927656
jiyun <jiyun_99@naver.com>
2022-11-02 19:17:54 +0900

    add mute mode

--- app/src/main/java/myapplication/app/pomodoro/MainActivity.kt
+++ app/src/main/java/myapplication/app/pomodoro/MainActivity.kt
@@ -2,11 +2,14 @@ package myapplication.app.pomodoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import myapplication.app.pomodoro.databinding.ActivityMainBinding
 import myapplication.app.pomodoro.vm.TimerViewModel
 
 class MainActivity : AppCompatActivity() {
+    //todo 무음모드일때 카운트다운 끝나면 어떻게 알려주지,,,?
+    //todo 무음모드 dataBinding 리팩토링하기
     private lateinit var binding: ActivityMainBinding
     private var viewModel: TimerViewModel = TimerViewModel()
 
@@ -21,7 +24,26 @@ class MainActivity : AppCompatActivity() {
         binding.seekBar.setOnSeekBarChangeListener(
             viewModel.createSeekBar()
         )
+        binding.volumeBtn.setOnClickListener{
+            controlSoundMode()
+        }
         initSounds()
+    }
+
+    private fun controlSoundMode(){
+        viewModel.isMuteMode.value = !viewModel.isMuteMode.value!!
+
+        if(viewModel.isMuteMode.value!!) binding.volumeBtn.setImageResource(R.drawable.ic_baseline_volume_off_24)
+        else binding.volumeBtn.setImageResource(R.drawable.ic_baseline_volume_up_24)
+
+        if(viewModel.isMuteMode.value!!) {
+//            imgResource.value = R.drawable.ic_baseline_volume_off_24
+            viewModel.soundPool.autoPause()
+        }
+        else if(viewModel.countState.value!! && viewModel.isMuteMode.value == false) {
+//            imgResource.value = R.drawable.ic_baseline_volume_up_24
+            viewModel.soundPool.autoResume()
+        }
     }
 
     private fun initSounds(){
@@ -31,7 +53,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        viewModel.soundPool.autoResume()
+        if(!viewModel.isMuteMode.value!!) viewModel.soundPool.autoResume()
     }
 
     override fun onPause() {
